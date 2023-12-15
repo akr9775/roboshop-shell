b83915b7683417b65dc97d0e214e177a91bcb8d2
akr9757 <anilkumareddy9757@gmail.com>
2023-12-15 16:48:34 +0530

    roboshop shell script

--- mongodb.sh
+++ mongodb.sh
@@ -4,14 +4,20 @@ source ${script_path}/common.sh
 
 
 echo -e '\e[32m >>>>>>>>>>>>> copy mongo repo <<<<<<<<<<<<\e[0m'
-cp ${script_path}/mongo.repo /etc/yum.repos.d/mongo.repo
+cp ${script_path}/mongo.repo /etc/yum.repos.d/mongo.repo &>>/tmp/roboshop.log
+func_stat_check $?
 
 echo -e '\e[32m >>>>>>>>>>>>> install mongod <<<<<<<<<<<<\e[0m'
-dnf install mongodb-org -y
+dnf install mongodb-org -y &>>/tmp/roboshop.log
+func_stat_check $?
 
 echo -e '\e[32m >>>>>>>>>>>>> update listen address <<<<<<<<<<<<\e[0m'
-sed -i -e "s|127.0.0.1|0.0.0.0|" /etc/mongod.conf
+sed -i -e "s|127.0.0.1|0.0.0.0|" /etc/mongod.conf &>>/tmp/roboshop.log
+func_stat_check $?
 
 echo -e '\e[32m >>>>>>>>>>>>> start mongod <<<<<<<<<<<<\e[0m'
-systemctl enable mongod
-systemctl restart mongod
+systemctl enable mongod &>>/tmp/roboshop.log
+systemctl restart mongod &>>/tmp/roboshop.log
+func_stat_check $?
+
+
